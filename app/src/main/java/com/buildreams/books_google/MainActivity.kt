46a06597e65ae3edfd49ece661d204dcf0067f8f
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2022-12-04 00:01:05 -0500

    add rx library and  create three buttons for experients

--- app/src/main/java/com/buildreams/books_google/MainActivity.kt
+++ app/src/main/java/com/buildreams/books_google/MainActivity.kt
@@ -3,11 +3,15 @@ package com.buildreams.books_google
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.buildreams.books_google.ui.theme.BooksgoogleTheme
@@ -22,9 +26,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    buttonList()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun buttonList() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { }) {
+            Text(text = "Accept")
+        }
+        Button(onClick = { }) {
+            Text(text = "Reject")
+        }
+        Button(onClick = { }) {
+            Text(text = "Thinking")
         }
     }
 }
